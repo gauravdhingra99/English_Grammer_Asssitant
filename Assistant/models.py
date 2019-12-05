4c89ec4b5dc91d40f6e7f936f89a9d1e2daaf056
gaurav <gauravdhingra9999@gmail.com>
2019-12-05 15:28:15 +0530

    update

--- Assistant/models.py
+++ Assistant/models.py
@@ -1,3 +1,9 @@
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-# Create your models here.
+class Sentence(models.Model):
+    query = models.CharField(max_length=200)
+    data = JSONField()
+
+    def __str__(self):
+        return self.query
